09054fd2867c2260c4f95f36e911fec34601531d
alexstoilov <108930312+alexmonovski@users.noreply.github.com>
2023-08-12 13:23:10 +0300

    null

--- client/Angular-Graduation-Project/src/app/authentication/register/register.component.ts
+++ client/Angular-Graduation-Project/src/app/authentication/register/register.component.ts
@@ -1,3 +1,5 @@
+import { ITopic } from './../../shared/interfaces/itopic';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
 import { Component, OnInit, inject } from '@angular/core';
 import {
   FormGroup,
@@ -5,6 +7,11 @@ import {
   FormBuilder,
   Validators,
 } from '@angular/forms';
+import { MatChipInputEvent } from '@angular/material/chips';
+import { passwordMatchValidator } from '../services/password-match-validator.service';
+import { ApiCallsService } from 'src/app/core/services/api-calls.service';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { startWith, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-register',
@@ -12,7 +19,25 @@ import {
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-  // the form
+  topicDocs = [];
+  topics: string[] = [];
+
+  removeTopic(topic: string) {
+    const index = this.topics.indexOf(topic);
+    if (index >= 0) {
+      this.topics.splice(index, 1);
+      this.announcer.announce(`removed ${topic}`);
+    }
+  }
+
+  add(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+    if (value) {
+      this.topics.push(value);
+    }
+    event.chipInput!.clear();
+  }
+
   formGroup!: FormGroup;
   get formArray(): AbstractControl | null {
     return this.formGroup.get('formArray');
@@ -25,22 +50,45 @@ export class RegisterComponent implements OnInit {
           email: ['', [Validators.required, Validators.email]],
         }),
         // add same pass validation
-        this._formBuilder.group({
-          password: ['', [Validators.required]],
-          repass: ['', [Validators.required]],
-        }),
+        this._formBuilder.group(
+          {
+            password: ['', [Validators.required]],
+            repass: ['', [Validators.required]],
+          },
+          {
+            validators: passwordMatchValidator(),
+          }
+        ),
         this._formBuilder.group({
           topics: [[]],
         }),
       ]),
     });
+
+    this.apiCalls.getAllTopics().subscribe({
+      next: (data) => {
+        for (const dataObj of data.topics) {
+          this.topics.push(dataObj.title);
+        }
+      },
+      error: (err) => console.log(err),
+      complete: () => console.log('Successfully fetched resources.'),
+    });
   }
 
   // масив от 3 обекта, за всяка група.
   onSubmit(): void {
-    const formData = this.formArray?.value;
-    console.log(formData);
+    if (this.formGroup.valid) {
+      const formData = this.formArray?.value;
+      console.log(formData);
+    } else {
+      console.log('Form has errors.');
+    }
   }
 
-  constructor(private _formBuilder: FormBuilder) {}
+  constructor(
+    private _formBuilder: FormBuilder,
+    private announcer: LiveAnnouncer,
+    private apiCalls: ApiCallsService
+  ) {}
 }
--- client/Angular-Graduation-Project/src/app/authentication/services/password-match-validator.service.ts
+++ client/Angular-Graduation-Project/src/app/authentication/services/password-match-validator.service.ts
@@ -1,11 +1,20 @@
-import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+import {
+  AbstractControl,
+  FormGroup,
+  ValidationErrors,
+  ValidatorFn,
+} from '@angular/forms';
 export function passwordMatchValidator(): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
-    const password = control.get('password');
-    const repass = control.get('repass');
+    const formGroup = control as FormGroup;
+    const password = formGroup.get('password');
+    const repass = formGroup.get('repass');
+
     if (password && repass && password.value !== repass.value) {
+      console.log('passwords are different');
       return { passwordMismatch: true };
     } else {
+      console.log('passwords are the same');
       return null;
     }
   };
